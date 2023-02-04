883b91b710793def9b6779eeba0cd6d23a4f3da2
Мамасали уулу Нурлан <n-bulash@mail.ru>
2023-02-05 02:02:45 +0600

    Домашка от Мамасали уулу Нурлан

---
+++ domashka1/Program.cs
@@ -0,0 +1,105 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+/*System.Console.WriteLine("Enter numer 1");
+
+string strNumber1 = Console.ReadLine();
+
+int number1 = Convert.ToInt32(strNumber1);
+
+System.Console.WriteLine("Enter numer 2");
+
+string strNumber2 = Console.ReadLine();
+
+int number2 = Convert.ToInt32(strNumber2);
+
+if (number1 > number2)
+{
+    System.Console.WriteLine($"Больше" + number1 + ", Меньше" + number2);
+} 
+else
+{
+    System.Console.WriteLine($" Больше " + number2 + ", Меньше " + number1);
+}*/
+
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+/*System.Console.WriteLine("Enter numer 1");
+
+int number1 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Enter numer 2");
+
+int number2 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Enter numer 3");
+
+int number3 = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if (number1 > max)
+{
+    max = number1;
+}
+
+if (number2 > max)
+{
+    max = number2;
+}
+
+if (number3 > max)
+{
+    max = number3;
+}
+
+System.Console.WriteLine($" Максимальное " + max );*/
+
+
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+/*System.Console.WriteLine("Enter numer: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int sum = number % 2;
+if (sum == 0)
+{
+
+    System.Console.WriteLine(" Да ");
+}
+else
+{
+    System.Console.WriteLine(" Нет ");
+}*/
+
+/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+/*System.Console.Write("Enter numer: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int number2 = 2;
+
+if(number > 1)
+{   
+    while(number2 <= number)
+    {
+    System.Console.Write(number2 + " ");
+    number2 = number2 + 2;
+    }
+}*/
