37932da531c2efef97567b760c165e16d3f2320a
Мамасали уулу Нурлан <n-bulash@mail.ru>
2023-02-13 21:02:54 +0600

    Домашка4 от Мамсали уулу Нурлан

---
+++ domashka4/Program.cs
@@ -0,0 +1,149 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16*/
+
+/*Console.WriteLine("Entre numberA ");
+int numA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre numberB ");
+int numB = int.Parse(Console.ReadLine());
+
+if (numA < 0 || numB < 0) 
+{
+    System.Console.WriteLine("Ошибка");
+}
+else
+{
+    System.Console.WriteLine(result(numA, numB));
+}
+int result(int A, int B)
+{
+    int sum = A;
+    
+    for(int count = 1; B > count; count++)
+    {
+        sum *= A;
+        
+    }
+    return sum;
+}*/
+/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+/*Console.WriteLine("Entre number ");
+
+int num = int.Parse(Console.ReadLine());
+
+if (num < 0) 
+{
+    System.Console.WriteLine("Ошибка");
+}
+else
+{
+    System.Console.WriteLine(result(num));
+}
+int result(int A)
+{
+    int sum = 0;
+    
+    while (A > 0)
+    {
+        int num = A % 10;
+        A = A / 10;
+        sum = sum + num;
+
+    }
+   
+    return sum;
+}*/
+/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+
+Console.Write("Введите ряд чисел, разделенных запятой : ");
+string? seriesOfNumbers = Console.ReadLine();
+
+seriesOfNumbers = seriesOfNumbers + ",";    
+
+// функция удаления пробелов из строки 
+string RemovingSpaces (string series){
+  string seriesNew = "";
+  for (int i = 0; i < series.Length; i++)
+  {
+    if (series[i] != ' ') 
+    {
+      seriesNew += series[i];
+    }
+  }
+  return seriesNew;
+}
+
+//  функция  проверки на правильность ввода 
+void СheckNumber2 (int  series){
+
+      if (series == '0'||series == '1'||series == '2'
+      ||series == '3'||series == '4'||series == '5'||series == '6'
+      ||series == '7'||series == '8'||series == '9'||series == ','
+      ||series == '-')
+      {
+      }
+        else {
+          Console.WriteLine($"Ошибка ввода  символа. Вводи цифры.");
+
+      }
+}
+
+// функция  создания и заполнения массива из строки
+int[] ArrayOfNumbers(string seriesNew){ 
+
+  int[] arrayOfNumbers = new int[1];   
+
+  int j =0;
+
+  for (int i = 0; i < seriesNew.Length; i++){
+    string seriesNew1 = "";
+
+    while (seriesNew[i] != ',' && i < seriesNew.Length){
+      seriesNew1 += seriesNew[i];
+      СheckNumber2(seriesNew[i]);
+      i++;
+    }
+    arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);   
+    if (i < seriesNew.Length-1){
+      arrayOfNumbers = arrayOfNumbers.Concat(new int[] {0}).ToArray();    
+    }
+    j++;
+  }
+  return arrayOfNumbers;
+}
+
+// функция  вывода массива на печать 
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  Console.Write("[");
+  while(index < count){
+    Console.Write(coll[index]);
+    index++;
+    if (index < count){
+      Console.Write(", ");
+    }
+  }
+  Console.Write("]");
+} 
+
+
+string seriesNew = RemovingSpaces(seriesOfNumbers);
+
+int[] arrayOfNumbers =  ArrayOfNumbers(seriesNew);
+
+PrintArry(arrayOfNumbers);
